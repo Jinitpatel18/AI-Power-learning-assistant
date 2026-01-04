ccacb2c9c1cc76ced02409399c2738672b839d87
Jinitpatel18 <jinitpatel188@gmail.com>
2026-01-04 13:51:34 +0530

    Final

--- frontend/ai-learning assistant/src/utils/axiosInstance.js
+++ frontend/ai-learning assistant/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPaths";
+// import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: '/api',
     timeout: 8000,
     headers: {
         "Content-Type":"application/json",
