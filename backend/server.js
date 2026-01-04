82c5cbe1ae279c160d1f5bee12e00d54ac76c47e
Jinitpatel18 <jinitpatel188@gmail.com>
2026-01-04 13:16:40 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-// app.options('*', cors());
+app.options('*', cors());
 // app.use((req, res, next) => {
 //     if (req.method === 'OPTIONS') {
 //         res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
